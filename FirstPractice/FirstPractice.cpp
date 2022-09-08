d9d74e25a710080bef7282c738c1129f2d2e7c11
student <student@C246U54>
2022-09-08 13:09:24 +0300

    Добавьте файлы проекта.

---
+++ FirstPractice/FirstPractice.cpp
@@ -0,0 +1,13 @@
+﻿// FirstPractice.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <stdio.h>
+using namespace std;
+
+
+
+int main()
+{
+    
+}
+
